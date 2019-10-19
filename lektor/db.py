f1d2cfa3e32f91c6cd1d56dd6eff5aa2c8861cd4
jossnaz <lukas.meier@gmail.com>
2019-10-18 20:09:16 -0500

    esoar_white_paper_2019.pdf.lr remove

--- lektor/db.py
+++ lektor/db.py
@@ -1501,6 +1501,7 @@ class Pad(object):
 
     def resolve_url_path(self, url_path, include_invisible=False,
                          include_assets=True, alt_fallback=True):
+        print('resolve url path enter ' + str(url_path))
         """Given a URL path this will find the correct record which also
         might be an attachment.  If a record cannot be found or is unexposed
         the return value will be `None`.
@@ -1528,6 +1529,7 @@ class Pad(object):
 
             rv = node.resolve_url_path(pieces)
             if rv is not None and (include_invisible or rv.is_visible):
+                print('return  ' + str(url_path) + ' rv: ' +  str(rv))
                 return rv
 
         if include_assets:
@@ -1535,6 +1537,7 @@ class Pad(object):
                 rv = asset_root.resolve_url_path(pieces)
                 if rv is not None:
                     break
+            print('return  ' + str(url_path) + ' rv: ' +  str(rv))
             return rv
         return None
 
